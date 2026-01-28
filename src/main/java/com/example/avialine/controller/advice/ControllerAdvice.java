1005f918cf7e507cb55883461c4bb9d0e06c77ae
Bekzhan <spotifyprm135@gmail.com>
2026-01-28 19:00:56 +0600

    added global method handlers and some fixes

--- src/main/java/com/example/avialine/controller/advice/ControllerAdvice.java
+++ src/main/java/com/example/avialine/controller/advice/ControllerAdvice.java
@@ -1,6 +1,6 @@
 package com.example.avialine.controller.advice;
 
-import com.example.avialine.exception.UserNotFoundException;
+import com.example.avialine.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,5 +15,34 @@ public class ControllerAdvice {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    @ExceptionHandler(PasswordDoNotMatchException.class)
+    public ResponseEntity<String> handlerPasswordDoNotMatchException(PasswordDoNotMatchException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handlerUserAlreadyExistsException(UserAlreadyExistsException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<String> handlerInvalidTokenException(InvalidTokenException e){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<String> handlerTokenExpiredException(TokenExpiredException e){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<String> handlerRoleNotFoundException(RoleNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(RoleAlreadyExistsException.class)
+    public ResponseEntity<String> handlerRoleAlreadyExistsException(RoleAlreadyExistsException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
 
 }
